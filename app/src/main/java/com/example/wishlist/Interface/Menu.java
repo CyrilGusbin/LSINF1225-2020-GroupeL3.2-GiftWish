db03e5a303b305ff6c673d3faa176dc3d5bb5ed9
HeyIamGG <62309283+HeyIamGG@users.noreply.github.com>
2020-05-03 18:31:11 +0200

    - Amélioration des commentaires pour l'interface "Menu". - Amélioration de la stabilité de l'application.

--- app/src/main/java/com/example/wishlist/Interface/Menu.java
+++ app/src/main/java/com/example/wishlist/Interface/Menu.java
@@ -20,19 +20,18 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        //Lorsque  l'on clique sur le bouton "Mes WishLists".
+        //Lorsque  l'on clique sur le bouton "Mes WishLists", l'application nous déplace vers l'interface "Mes WishLists".
         this.play = findViewById(R.id.wishlist);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent otherActivity = new Intent(getApplicationContext(), Friends.class);
+                Intent otherActivity = new Intent(getApplicationContext(), Liste_wishlists.class);
                 startActivity(otherActivity);
-                finish();
             }
         });
 
-        //Lorsque l'on clique sur le bouton "Mes amis".
+        //Lorsque l'on clique sur le bouton "Mes amis", l'application nous déplace vers l'interface "Mes amis".
         this.play = findViewById(R.id.amis);
 
         play.setOnClickListener(new View.OnClickListener() {
@@ -40,11 +39,10 @@ public class Menu extends AppCompatActivity {
             public void onClick(View view) {
                 Intent otherActivity = new Intent(getApplicationContext(), Friends.class);
                 startActivity(otherActivity);
-                finish();
             }
         });
 
-        //Lorsque l'on clique sur le bouton "Mon Profil".
+        //Lorsque l'on clique sur le bouton "Mon Profil", l'application nous déplace vers l'interface "Mon profil".
         this.play = findViewById(R.id.profil);
 
         play.setOnClickListener(new View.OnClickListener() {
@@ -52,11 +50,10 @@ public class Menu extends AppCompatActivity {
             public void onClick(View view) {
                 Intent otherActivity = new Intent(getApplicationContext(), Profile.class);
                 startActivity(otherActivity);
-                finish();
             }
         });
 
-        //Lorsque l'on clique sur le bouton "Déconnexion".
+        //Lorsque l'on clique sur le bouton "Déconnexion", l'application nous déplace vers l'interface "Connexion".
         this.play = findViewById(R.id.deconnexion);
 
         play.setOnClickListener(new View.OnClickListener() {
