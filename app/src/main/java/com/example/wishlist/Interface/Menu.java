cfec8030ef4f528263349e6f7a5823ce4e5fce09
HeyIamGG <62309283+HeyIamGG@users.noreply.github.com>
2020-05-04 18:13:11 +0200

    - Amélioration stabilité

--- app/src/main/java/com/example/wishlist/Interface/Menu.java
+++ app/src/main/java/com/example/wishlist/Interface/Menu.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.wishlist.R;
 
@@ -21,6 +22,11 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.pseudo_user);
+        textView.setText("Bienvenue, "+message);
 
         //Lorsque  l'on clique sur le bouton "Mes WishLists", l'application nous déplace vers l'interface "Mes WishLists".
         this.play = findViewById(R.id.wishlist);
