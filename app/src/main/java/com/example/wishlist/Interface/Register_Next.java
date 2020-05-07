5f971bfeb5aad7d7f679adcd81154116a2f1bc9b
gadupont <gatie.dupont@uclouvain.be>
2020-05-07 21:48:20 +0200

    initialisation les colonnes request et friend a "0" avec tous les utilisateurs existant lors de la création d'un user

--- app/src/main/java/com/example/wishlist/Interface/Register_Next.java
+++ app/src/main/java/com/example/wishlist/Interface/Register_Next.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.wishlist.DAO.UserDAO;
 import com.example.wishlist.R;
@@ -25,6 +27,10 @@ public class Register_Next extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register__next);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.error);
+        textView.setText(message);
     }
     public void CreateData(View view){
         userDao= new UserDAO(activity);
@@ -34,15 +40,26 @@ public class Register_Next extends AppCompatActivity {
         EditText age = findViewById(R.id.setupAge);
         String nom_bis= nom.getText().toString();
         String prenom_bis= prenom.getText().toString();
-        String age_bis= age.getText().toString();
-        Intent intent2= getIntent();
-        String pseudo_bis = intent2.getStringExtra(EXTRA_MESSAGE);
-        Log.e("pseudo", pseudo_bis);
-        int real_age= Integer.parseInt(age_bis);
-        if(userDao.AddDataDB(pseudo_bis, nom_bis, prenom_bis, age_bis)){
-            intent.putExtra(EXTRA_MESSAGE, pseudo_bis);
-            startActivity(intent);
+
+        try {
+            String age_bis= age.getText().toString();
+            Intent intent2= getIntent();
+            String pseudo_bis = intent2.getStringExtra(EXTRA_MESSAGE);
+            Log.e("pseudo", pseudo_bis);
+            int real_age= Integer.parseInt(age_bis);
+            if(userDao.AddDataDB(pseudo_bis, nom_bis, prenom_bis, age_bis)){
+                intent.putExtra(EXTRA_MESSAGE, pseudo_bis);
+                startActivity(intent);
+            }
         }
+        catch (Exception e){
+            Intent intent4 = new Intent(this, Register_Next.class);
+            intent4.putExtra(EXTRA_MESSAGE, "la valeur doit etre un nombre");
+            startActivity(intent4);
+            finish();
+        }
+
+
         }
 
 }
